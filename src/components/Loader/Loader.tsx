5d0ece3becc228c8c25aeb266cc36502afcb25df
piotrfijalkiewicz <piotr.fjj@gmail.com>
2025-06-19 21:57:34 +0200

    add spinner to Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './Loader.module.scss';
 
 export const Loader = () => {
-    return <div className={styles.loader}>Loading...</div>;
+    return <div className={styles.loader}><span>Loading</span><span className={styles.loader__spinner}></span></div>;
 };
